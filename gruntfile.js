f0a45b7481250f1e10b15837a7351a71ed0ae26c
Michael Bragg <michael@michaelbragg.net>
2015-02-03 08:58:32 +0000

    Initial setup

---
+++ gruntfile.js
@@ -0,0 +1,228 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+  //  Config
+    pkg: grunt.file.readJSON('package.json')
+
+   ,config: {
+      dev: {
+        options: {
+          variables: {
+            'dest': '_site'
+          }
+        }
+      },
+      stage: {
+        options: {
+          variables: {
+            'dest': 'beta'
+          }
+        }
+      },
+      deploy: {
+        options: {
+          variables: {
+            'dest': '2015'
+          }
+        }
+      }
+    }
+
+  //  Build Site
+
+   ,watch: {
+      files: ['src/**/*', '!src/_includes/bower_components/**/*']
+     ,tasks: ['dev']
+     ,options: {
+        livereload: true
+      }
+    }
+
+   ,express: {
+      dev: {
+        options: {
+          port: 3000,
+          hostname: 'localhost',
+          bases: '<%= grunt.config.get("dest") %>'
+        }
+      }
+    }
+
+   ,shell: {
+      jekyll_dev: {
+        command: 'jekyll build'
+      },
+      jekyll_stage: {
+        command: 'jekyll build --destination <%= grunt.config.get("dest") %> --config _config.stage.yml'
+      },
+      jekyll_deploy: {
+        command: 'jekyll build --destination <%= grunt.config.get("dest") %> --config _config.production.yml'
+      },
+      open: {
+        command: 'open ./<%= grunt.config.get("dest") %>'
+      }
+    }
+
+   ,copy: {
+      bower: {
+        files: [
+          { expand: true, flatten: true, cwd: 'src/_includes/bower_components', src: ['html5shiv/dist/html5shiv.js'], dest: '<%= grunt.config.get("dest") %>/static/js/lib'},
+          { expand: true, flatten: true, cwd: 'src/_includes/bower_components', src: ['respondJS/dest/respond.min.js'], dest: '<%= grunt.config.get("dest") %>/static/js/lib'}
+        ]
+      }
+    }
+
+   ,clean: {
+      files: ['<%= grunt.config.get("dest") %>']
+    }
+
+  // Compile
+
+   ,less: {
+      development: {
+        options: {
+          paths: ['src/static/css']
+        }
+       ,files: {
+          'src/static/css/global.css': ['src/_includes/less/global.less'],
+          'src/_includes/css/preload.css': ['src/_includes/less/preload.less']
+        }
+      }
+     ,production: {
+        options: {
+          compress: true
+         ,paths: ['src/static/css']
+        }
+       ,files: {
+          'src/static/css/global.css': ['src/_includes/less/global.less'],
+          'src/_includes/css/preload.css': ['src/_includes/less/preload.less']
+        }
+      }
+    }
+
+  // Validate
+
+   ,htmlhint: {
+      options: {
+        'tag-pair': true,
+        'tagname-lowercase': true,
+        'attr-lowercase': true,
+        'attr-value-double-quotes': true,
+        'doctype-first': true,
+        'spec-char-escape': true,
+        'id-unique': true,
+        'style-disabled': true,
+        'src-not-empty': true,
+        'img-alt-require': true
+      },
+      src: ['<%= grunt.config.get("dest") %>/**/*.html']
+
+    }
+
+   ,csslint: {
+      options: {
+        'adjoining-classes': false,
+        'box-model': false,
+        'box-sizing': false,
+        'regex-selectors': false,
+        'universal-selector': false,
+        'font-sizes': false  //  Until CSSLint has the option to set an ammount
+      },
+      src: ['<%= grunt.config.get("dest") %>/static/css/*.css']
+    }
+
+   ,jshint: {
+      options: {
+        browser: true,
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        indent: 2,
+        laxbreak: true,
+        laxcomma: true,
+        quotmark: 'single',
+        trailing: true,
+        undef: true,
+        globals: {
+          console: true,
+          module: true,
+          jQuery: true,
+          Modernizr: true
+        }
+      },
+      src: ['gruntfile.js', 'src/_includes/js/*.js']
+    }
+
+  // Optimise
+
+   ,imagemin: {
+      options: {
+        optimizationLevel: 3
+      },
+      dev: {
+        files: [{
+          expand: true,
+          cwd: '<%= grunt.config.get("dest") %>/static/gui',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: '<%= grunt.config.get("dest") %>/static/gui'
+        },
+        {
+          expand: true,
+          cwd: '<%= grunt.config.get("dest") %>/media',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: '<%= grunt.config.get("dest") %>/static/media'
+        }]
+      }
+    }
+
+   ,hashres: {
+      options: {
+        encoding: 'utf8',
+        fileNameFormat: '${name}.${hash}.${ext}',
+        renameFiles: true
+      },
+      image: {
+        src: ['<%= grunt.config.get("dest") %>/media/**/*.{png,jpg,gif,svg}','<%= grunt.config.get("dest") %>/static/**/*.{png,jpg,gif,svg}'],
+        dest: '<%= grunt.config.get("dest") %>/**/*.{html,css}',
+      },
+      css: {
+        src: ['<%= grunt.config.get("dest") %>/static/css/**/*.css'],
+        dest: '<%= grunt.config.get("dest") %>/**/*.html',
+      },
+      js: {
+        src: ['<%= grunt.config.get("dest") %>/static/js/**/*.js'],
+        dest: '<%= grunt.config.get("dest") %>/**/*.html',
+      }
+    }
+
+  });
+
+  // Tasks
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-config');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-hashres');
+  //grunt.loadNpmTasks('grunt-autoprefixer');
+
+  // Options
+
+  grunt.registerTask('default', ['dev', 'serve']);
+  grunt.registerTask('test', ['config:dev', 'htmlhint', 'csslint', 'jshint']);
+  grunt.registerTask('optim', ['imagemin']);
+  grunt.registerTask('dev', ['config:dev', 'clean', 'less:development', 'shell:jekyll_dev', 'copy']);
+  grunt.registerTask('serve', ['express', 'watch']);
+  grunt.registerTask('stage', ['config:stage', 'clean', 'less:production', 'shell:jekyll_stage', 'copy', 'hashres', 'optim']);
+  grunt.registerTask('deploy', ['config:deploy', 'clean', 'less:production', 'shell:jekyll_deploy', 'copy', 'hashres', 'optim']);
+
+};
---
+++ readme.md
@@ -0,0 +1,94 @@
+# Coventry Telegraph Business Awards 2014
+[http://ctbusinessawards.co.uk/2015/](http://www.ctbusinessawards.co.uk/2015/)
+
+## Description
+Website to support the 2015 Coventry Telegraph Business Awards
+
+## Dependencies
+
+- NodeJS
+  - Grunt
+  - LESS CSS
+  - Bower
+- Ruby
+  - Jekyll
+
+## Instructions
+
+These instructions expect that you have Ruby and NodeJS already installed.
+
+### Setup Development Area
+
+```
+bundle install
+npm install
+bower install
+```
+
+### Run Development Server
+
+```
+grunt
+```
+
+Visit the url [localhost:3000](http://localhost:3000/) to view the site.
+
+### Deploy Build
+
+#### Staging
+
+For testing and proofing we deploy to a staging server before deploying to production.
+
+```
+grunt stage
+```
+
+This deploys to the staging server [http://www.ctbusinessawards.co.uk/beta/](http://www.ctbusinessawards.co.uk/beta/).
+
+#### Production
+
+All commits and merges to the master branch that pass our CI tests will be automatically deployed to our production server.
+
+## Documentation
+
+During the Alpha/Beta stages, due to constant changes, documentation will be mainly written in-line. With a dedicated section being created at the first major release.
+
+### File Structure
+
+```
+|- _scripts              –  contains useful scripts to help with
+|                           development of this project
+|- _site                 –  compiled development files (not committed)
+|- web
+|  |- _includes          –  partial snippets of code to be used
+|  |                        in layouts
+|  |- _layouts           –  page layouts
+|  |- _posts             -  markdown files of news posts
+|  |- media              -
+|  |- static             -
+|  |- {**/*.txt,html,md} -  files for website
+|- _config.yml           -  Jekyll config file
+|- gruntfile.js
+|- package.json
+|- readme.md
+```
+
+## Report Issues
+
+If you spot any issues please create a ticket via GitHub's Issue Tracker. If the issue is security related please use the contact information below.
+
+## Contribute
+
+In lieu of a formal style guide, take care to maintain the existing coding style.
+
+## Contact
+
+[tmcreative@trinitymirror.com](mailto:tmcreative@trinitymirror.com)
+
+## License
+
+The source is opened for educational purposes. No rights are assigned to any downloads or forks.
+
+## Copyright
+
+Unless otherwise stated all code and content remain copyright &copy; Trinity Mirror. All rights reserved.
