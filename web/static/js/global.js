e375f1665d855a279162f16cff7f2ff6d8793042
Michael Bragg <michael@michaelbragg.net>
2015-02-12 15:44:20 +0000

    Add responsive navigation for site header

--- web/static/js/global.js
+++ web/static/js/global.js
@@ -5,4 +5,20 @@ sitemap:
 
 {% include bower_components/jquery/dist/jquery.min.js %}
 
+/**
+ * Responsive Navigation
+ */
+(function ( $ ) {
+
+  $(document).ready(function() {
+
+
+    $("#js-menu__icon").click(function () {
+      $("#js-nav__main").toggleClass("is-active");
+    });
+
+  });
+
+}(jQuery));
+
 {% include bower_components/FitVids/jquery.fitvids.js %}
